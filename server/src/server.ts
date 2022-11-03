16a4f471c1293a3a36b594a87db26a009752349b
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-03 09:49:30 -0300

    generate erd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -10,13 +11,17 @@ async function bootstrap() {
     logger: true,
   })
 
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
