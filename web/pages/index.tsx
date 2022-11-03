2f137e490cf56e7ebfed9544c7803b39d70e1c9f
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-03 14:56:28 -0300

    setup web

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+interface HomeProps {
+  count: number
+}
+
+export default function Home({ count }: HomeProps) {
+  return <h1>Contagem: {count}</h1>
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.count,
+    },
+  }
 }
