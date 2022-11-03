a2bd8d52313df7ea7ddf13d8eb6aaea9a29a5428
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-03 15:38:51 -0300

    config setup mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider, Center, Text } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { THEME } from './src/styles/theme'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}>
+          Open up App.js to start working on your app!
+        </Text>
+
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
+  )
+}
