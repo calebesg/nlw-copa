f2c34b4a81812ecd27c9a9814221ba4c9160082a
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-04 19:49:36 -0300

    create signin screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Icon } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
 
 import { Button } from '../components/Button'
@@ -7,14 +7,20 @@ import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
-        title="entrar com google"
         type="SECONDARY"
+        title="entrar com google"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={21}
       />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+        de sua conta.
+      </Text>
     </Center>
   )
 }
