45bf7f1e6ba4c71fb40e015e4843cbd5408c13ff
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-10 09:56:26 -0300

    list my pools

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -108,4 +108,43 @@ export async function poolRoutes(fastify: FastifyInstance) {
       return reply.status(201).send()
     }
   )
+
+  fastify.get('/pools', { onRequest: [authenticate] }, async request => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        participants: {
+          some: {
+            userId: request.user.sub,
+          },
+        },
+      },
+      include: {
+        _count: {
+          select: {
+            participants: true,
+          },
+        },
+        participants: {
+          select: {
+            id: true,
+
+            user: {
+              select: {
+                id: true,
+                avatarUrl: true,
+              },
+            },
+          },
+        },
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    })
+
+    return { pools }
+  })
 }
