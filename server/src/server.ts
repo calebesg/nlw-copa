019ca2b8756f29645ddcfff2a8cbcc30b06efbd7
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-11 09:06:29 -0300

    get a games

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import { poolRoutes } from './routes/pool'
 import { userRoutes } from './routes/user'
 import { guessRoutes } from './routes/guess'
 import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -24,6 +25,7 @@ async function bootstrap() {
   await fastify.register(userRoutes)
   await fastify.register(guessRoutes)
   await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
 
   await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ })
 }
