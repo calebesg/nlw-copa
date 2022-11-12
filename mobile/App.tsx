5374a357fbe96f466f21f5872cb4dac9d3fd55b8
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-12 10:27:52 -0300

    create tab navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,8 +8,7 @@ import {
 
 import { AuthContextProvider } from './src/contexts/AuthContext'
 
-import { SignIn } from './src/screens/SignIn'
-import { New } from './src/screens/New'
+import { Routes } from './src/routes'
 import { Loading } from './src/components/Loading'
 
 import { THEME } from './src/styles/theme'
@@ -30,7 +29,7 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   )
