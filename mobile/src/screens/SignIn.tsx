820dedc3bd45b74920e6f8b3d0a442c58654be3f
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-13 10:46:42 -0300

    redirect logged user

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -7,7 +7,7 @@ import { Button } from '../components/Button'
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
-  const { signIn, user } = useAuth()
+  const { signIn, isUserLoading } = useAuth()
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
@@ -19,6 +19,8 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={21}
         onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: 'white' } }}
       />
 
       <Text color="white" textAlign="center" mt={4}>
