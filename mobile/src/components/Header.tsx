c4621ff81a94931ac4fddafa7d4c706fd35c5d9e
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-16 09:50:35 -0300

    create details and share pool

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -8,12 +8,14 @@ interface Props {
   title: string
   showBackButton?: boolean
   showShareButton?: boolean
+  onShare?: () => void
 }
 
 export function Header({
   title,
   showBackButton = false,
   showShareButton = false,
+  onShare,
 }: Props) {
   const { navigate } = useNavigation()
 
@@ -44,7 +46,11 @@ export function Header({
           {title}
         </Text>
 
-        {showShareButton ? <ButtonIcon icon={Export} /> : <EmptyBoxSpace />}
+        {showShareButton ? (
+          <ButtonIcon icon={Export} onPress={onShare} />
+        ) : (
+          <EmptyBoxSpace />
+        )}
       </HStack>
     </HStack>
   )
