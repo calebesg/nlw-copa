5140f8fd2f2395f61786eac512b1b5fd17f6405b
CalebeSG <calebeguimaraes10@gmail.com>
2022-11-19 08:54:24 -0300

    config dotenv

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -29,8 +29,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   const [isUserLoading, setIsUserLoading] = useState(false)
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId:
-      '313390843828-b22pk2tka34lsp9ng2n259bg62mdai96.apps.googleusercontent.com',
+    clientId: process.env.CLIENT_ID,
     redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
     scopes: ['profile', 'email'],
   })
